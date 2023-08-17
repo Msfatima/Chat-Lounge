2047f3374dfb7c8e1e9bbd4cd54997c3ccc23b71
Msfatima <eng.fatimah1@gmail.com>
2023-08-17 15:36:47 +0300

    First Commit

---
+++ Chat-Loung/Models/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Chat_Loung.Models.Hubs
+{
+    //A hub is a class that serves as a high-level pipeline that handles client-server communication.
+    //The ChatHub class inherits from the SignalR Hub class.
+    //The Hub class manages connections, groups, and messaging.
+    public class ChatHub : Hub
+    {
+        //  The SendMessage method can be called by a connected client to send a message to all clients.JavaScript client code that calls the method is shown later in the tutorial.SignalR code is asynchronous to provide maximum scalability.
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+     
+    }
+}
